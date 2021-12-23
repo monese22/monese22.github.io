717840f5b33727dcbd14c3aaca3af146001c073a
Oungseik <mhemaungthuwin@gmail.com>
2021-12-23 10:52:40 +0630

    refactor: delete unnecessary images and implement Header

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,31 +1,53 @@
+import Image from 'next/image';
 import * as React from 'react';
 
-import UnstyledLink from '@/components/links/UnstyledLink';
+import UnstyledLink from '../links/UnstyledLink';
 
-const links = [
-  { href: '/', label: 'Route 1' },
-  { href: '/', label: 'Route 2' },
-];
+// this data must be redesign
+const navigations = ['Series', 'Shorts', 'Genere', 'Years'];
 
 export default function Header() {
   return (
-    <header className='sticky top-0 z-50 bg-white'>
-      <div className='layout flex justify-between items-center h-14'>
-        <UnstyledLink href='/' className='font-bold hover:text-gray-600'>
-          Home
-        </UnstyledLink>
-        <nav>
-          <ul className='flex justify-between items-center space-x-4'>
-            {links.map(({ href, label }) => (
-              <li key={`${href}${label}`}>
-                <UnstyledLink href={href} className='hover:text-gray-600'>
-                  {label}
-                </UnstyledLink>
-              </li>
-            ))}
-          </ul>
-        </nav>
+    <header className='bg-mnHeaderBg text-mnWhite flex justify-between items-center px-6 py-2 mx-auto'>
+      <UnstyledLink href='/' className='flex items-center'>
+        <Image
+          src='/images/MoneseLogo.svg'
+          width={40}
+          height={40}
+          alt='monese-logo'
+          className='rounded-md'
+        />
+        <span className='pl-2 text-2xl font-semibold uppercase'>Monsese</span>
+      </UnstyledLink>
+
+      <div className='relative w-1/4 h-10 rounded-full'>
+        <div className='absolute'>
+          <svg
+            className='text-white/60 ml-2 w-8 h-10 scale-50 translate-y-1 fill-current'
+            xmlns='http://www.w3.org/2000/svg'
+            spacing='preserve'
+          >
+            <path d='M20.194 3.46c-4.613-4.613-12.121-4.613-16.734 0-4.612 4.614-4.612 12.121 0 16.735 4.108 4.107 10.506 4.547 15.116 1.34.097.459.319.897.676 1.254l6.718 6.718a2.498 2.498 0 0 0 3.535 0 2.496 2.496 0 0 0 0-3.535l-6.718-6.72a2.5 2.5 0 0 0-1.253-.674c3.209-4.611 2.769-11.008-1.34-15.118zm-2.121 14.614c-3.444 3.444-9.049 3.444-12.492 0-3.442-3.444-3.442-9.048 0-12.492 3.443-3.443 9.048-3.443 12.492 0 3.444 3.444 3.444 9.048 0 12.492z' />
+          </svg>
+        </div>
+        <form action='#'>
+          <input
+            type='text'
+            placeholder='Search'
+            id='search'
+            className='pl-10 w-full bg-transparent rounded-full'
+          />
+        </form>
       </div>
+
+      {/* This part need to change when implementing data */}
+      <nav className='text-mnWhite/50 text-semibold space-x-2'>
+        {navigations.map((nav) => (
+          <UnstyledLink href='#' key={nav} className='hover:text-white'>
+            {nav}{' '}
+          </UnstyledLink>
+        ))}
+      </nav>
     </header>
   );
 }
