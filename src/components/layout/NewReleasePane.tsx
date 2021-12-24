ed8647d20b1c79b44c257d421d89a964e68f93c6
Oungseik <mhemaungthuwin@gmail.com>
2021-12-24 10:34:14 +0630

    fix: add some styling to this component

--- src/components/layout/NewReleasePane.tsx
+++ src/components/layout/NewReleasePane.tsx
@@ -2,16 +2,19 @@ import NextImage from '../NextImage';
 
 export default function NewReleasePane() {
   return (
-    <div>
+    <div className='relative'>
       {/* image name need to be dynamic and need to add hyper link */}
-      <span>New releases:</span>
-      <div className='h-[400px] w-[1160px] overflow-hidden rounded-xl'>
+      <span className='text-lg font-semibold'>New releases:</span>
+      <div className='h-[400px] w-[1160px] overflow-hidden mt-1 rounded-xl'>
         <NextImage
           src='/images/posters/shang-chi-1.png'
           width={1160}
           height={400}
           alt='shang-chi'
         />
+        <span className='absolute bottom-2 left-2 text-2xl font-semibold'>
+          Shang-Chi and The Legend of The Ten Rings
+        </span>
       </div>
     </div>
   );
