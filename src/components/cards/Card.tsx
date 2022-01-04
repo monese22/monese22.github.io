b529db945efda36f8df15340babdba88386326ac
Oungseik <mhemaungthuwin@gmail.com>
2022-01-04 07:59:55 +0630

    refactor: add type information and render the component based on the props

--- src/components/cards/Card.tsx
+++ src/components/cards/Card.tsx
@@ -1,30 +1,33 @@
 import UnstyledLink from '../links/UnstyledLink';
 import NextImage from '../NextImage';
 
-// interface CardProps {
-//   href: string,
-//   rating: number,
-//   poster: string,
-//   name: string,
-// }
+type CardProps = {
+  href: string; // route to detail page
+  rating: number; // imdb rating number
+  posterPath: string; // path which is used to render the image
+  posterName: string; // used in image `alt` attribute
+  title: string; // title of the movie
+};
 
-export default function Card() {
+export default function Card(props: CardProps) {
   return (
     <UnstyledLink
-      href='#'
+      href={props.href}
       className='shadow-gray-700 w-[230px] block h-auto text-center rounded-md shadow'
     >
       <div className='relative'>
         <span className='absolute top-2 left-2 z-10 p-1 text-xs text-black bg-yellow-300 rounded'>
-          IMDB - 8.5
+          IMDB - {props.rating}
         </span>
+
         <NextImage
-          src='/images/posters/SpiderMan 1.png'
+          src={props.posterPath}
           width={230}
           height={340}
-          alt='Spyder Man Poster'
+          alt={props.posterName}
         />
-        <span className='block py-2'>Spyder-Man - Far From Home (2020)</span>
+
+        <span className='block py-2'>{props.title}</span>
       </div>
     </UnstyledLink>
   );
