bd36f2b3300319cbbc6d57e57011c4336588801f
Oungseik <mhemaungthuwin@gmail.com>
2022-01-24 20:15:28 +0630

    style: add some style classes

--- src/components/layout/MainAds.tsx
+++ src/components/layout/MainAds.tsx
@@ -1,6 +1,6 @@
 export default function MainAds() {
   return (
-    <div className='border-mnWhite flex flex-col justify-center items-center w-full h-48 border-2'>
+    <div className='border-mnWhite text-mnWhite flex overflow-hidden flex-col justify-center items-center w-full h-48 rounded-lg border-2'>
       <h2>Ads Available!</h2>
       <p>You can contact us from Facebook page chat box</p>
     </div>
