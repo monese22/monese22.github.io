10df99879d672b265fe083bd84186db64f314173
Oungseik <mhemaungthuwin@gmail.com>
2022-01-26 07:45:39 +0630

    feat: fetch the contentHtml data

--- src/lib/posts.ts
+++ src/lib/posts.ts
@@ -1,6 +1,8 @@
 import fs from 'fs';
 import matter from 'gray-matter';
 import path from 'path';
+import { remark } from 'remark';
+import html from 'remark-html';
 
 // TODO
 // There will be a lot of duplication when implememt this for series
@@ -17,18 +19,23 @@ export type MovieData = {
     large: string;
     small: string;
   };
+  contentHtml: string;
 };
 
 const POSTS_PATH = path.join(process.cwd(), 'src/posts');
 
-export function getMovieData(id: string): MovieData {
+export async function getMovieData(id: string): Promise<MovieData> {
   const fullPath = path.join(POSTS_PATH, `/movies/${id}.md`);
   const fileContent = fs.readFileSync(fullPath, 'utf8');
-  const { data } = matter(fileContent);
+  const { data, content } = matter(fileContent);
+
+  const processedContent = await remark().use(html).process(content);
+  const contentHtml = processedContent.toString();
 
   return {
     id,
     ...data,
+    contentHtml,
   } as MovieData;
 }
 
